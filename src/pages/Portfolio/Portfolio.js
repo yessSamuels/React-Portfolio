6c62eb1f038b38bc0305348c58cac40ce2caa16f
Yess Samuels <73135039+yessSamuels@users.noreply.github.com>
2021-02-01 13:36:41 -0800

    Update Portfolio.js

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -25,34 +25,34 @@ function Portfolio() {
 
               <div className="row">
                 <div className="col-xl-6">
-                  <a href="https:githublink" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={WeatherDashboard} alt="screenshot of weather dashboard with populated data of Tokyo"></img></a>
+                  <a href="https:githublink" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={} alt="LAND PAD"></img></a>
                   <a href="https:githublink" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">name</p></a>                        
                 </div>
                 <div className="col-xl-6">
-                  <a href="herokulink" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={IceBox} alt="landpad"></img></a>
-                  <a href="shoelink" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">name</p></a>
+                  <a href="" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={} alt=""></img></a>
+                  <a href="" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="">name</p></a>
                 </div>
               </div>
 
               <div className="row">
               <div className="col-xl-6">
-                  <a href="heroku" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={Landpad} alt="screenshot of login page of LAND PAD"></img></a>
-                  <a href="https://github.com/mfitzwinick/landpad" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">LAND PAD</p></a>
+                  <a href="" target="_blank" rel="noopener noreferrer"><img className="" src={} alt=""></img></a>
+                  <a href="" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="">name</p></a>
                 </div>
                 <div className="col-xl-6">
-                  <a href="link" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={CodeQuiz} alt="what is call"></img></a>
-                  <a href="link" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">name</p></a>
+                  <a href="" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={} alt="what is call"></img></a>
+                  <a href="" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="">name</p></a>
                 </div>
               </div>
 
               <div className="row">
               <div className="col-xl-6">
-                  <a href="project1" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={SearchAndRescue} alt="name"></img></a>
-                  <a href="project1" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">project1</p></a>
+                  <a href="" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={} alt="name"></img></a>
+                  <a href="" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title"></p></a>
                 </div>
                 <div className="col-xl-6">
-                  <a href="hwlink" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={EmpDir} alt="name"></img></a>
-                  <a href="hwlink" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">name</p></a>
+                  <a href="" target="_blank" rel="noopener noreferrer"><img className="portfolio-image" src={} alt="name"></img></a>
+                  <a href="" className="repo-link" target="_blank" rel="noopener noreferrer"><p className="project-title">name</p></a>
                 </div>
               </div>
 
